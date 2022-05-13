200cf7bad7a9738387259926a80055f503047983
Zach Weston <zachweston@Zachs-MacBook-Pro.local>
2022-05-12 22:05:30 -0600

    work in progress

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const leaderboard = $("#leaderboard-btn");
+const start = $("#start-btn");
+const quizContainer = $(".quiz-container");
+const questionDiv = $("#question-div");
+const question = $("#question");
+const choicesUl = $(".choices-ul");
+let startTime = 100;
+let score = 0;
+const answerDeduction = 5;
+
+//Questions array
+const questionsArray = [
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    },
+    {
+        question: "",
+        choices: ["", "", "", ""],
+        answer: ""
+    }
+]
+
+
+
+leaderboard.on("click", );
+
+
+start.on("click", );
